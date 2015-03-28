d220f5ca226da8f737dc7c357bfbe964eede4639
dcaro <dcaro@microsoft.com>
2015-03-28 14:55:27 -0600

    remove ssl verification

---
+++ BoothDemo/AzureRMdeploy/Deploy ARM VMs.ps1
@@ -0,0 +1,9 @@
+﻿# Deploy Chef VM 
+#### Deployment of MRP ######
+$RGName = "ChefBooth1"
+$DeploymentName = "ChefBoothDeploy1"
+New-AzureResourceGroup -Name $RGName -Location 'West US'
+New-AzureResourceGroupDeployment -Name chefdeploy1 -ResourceGroupName $RGName -TemplateFile '.\DeployWebApp.json' -TemplateParameterFile '.\DeployWebApp.parameters.json' -Verbose
+Get-AzurePublicIpAddress -ResourceGroupName $RGName
+
+Remove-AzureResourceGroup "ChefBooth1"
